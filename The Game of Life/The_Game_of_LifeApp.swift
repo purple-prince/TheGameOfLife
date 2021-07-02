31b9c7ea0ea8e2748e01aed68eb325bfb0ce7c99
purpleprince44 <65055638+purpleprince44@users.noreply.github.com>
2021-07-02 02:56:21 -0500

    Initial Commit

---
+++ The Game of Life/The_Game_of_LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Game_of_LifeApp.swift
+//  The Game of Life
+//
+//  Created by Charlie Reeder on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct The_Game_of_LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
